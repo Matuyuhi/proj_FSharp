b1e3ede1b67ef98e46a777e8167b6c3095d83fa5
yuhi yamane <bird9.yuhi@gmail.com>
2024-05-01 16:14:57 +0900

    :sparkles: Add Git mappings and update Program.fs code

--- Program.fs
+++ Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// 変数宣言
+let message = "Hello, F#"
+
+// 関数定義
+let square x = x * x
+
+// パイプライン操作
+let squaredResults = [1;2;3;4;5] 
+                    |> List.map square 
+
+// タプル
+let person = ("John", 32)
+
+// レコード型
+type Student = { Name : string; Age : int }
+let student = { Name = "Amy"; Age = 20 }
+
+printfn $"{student.Name} {student.Age}"
